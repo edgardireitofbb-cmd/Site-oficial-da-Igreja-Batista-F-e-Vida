97f59b9c768315981ef16021fe596990f0414bad
Edgar Menezes <edgardireito@gmail.com>
2026-07-20 22:36:13 -0300

    Atualização do Pastor IA e melhorias no site

--- script.js
+++ script.js
@@ -82,3 +82,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+function copiarPix() {
+
+    const chave = document.getElementById("chavePix").innerText;
+
+    navigator.clipboard.writeText(chave);
+
+    alert("✅ Chave PIX copiada com sucesso!");
+
+}
